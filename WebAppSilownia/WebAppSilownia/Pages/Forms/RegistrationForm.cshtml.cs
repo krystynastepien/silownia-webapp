964e65635ce771b70896586b16c18a59e9a253d0
Krystyna S <evenesk@gmail.com>
2020-01-13 22:42:26 +0100

    register form page
    todo:
    send data to database
    add validation

---
+++ WebAppSilownia/WebAppSilownia/Pages/Forms/RegistrationForm.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppSilownia.Models;
+
+namespace WebAppSilownia
+{
+    public class RegistrationFormModel : PageModel
+    {
+        [BindProperty]
+        public RegisterModel Register { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            //TODO: add validation of whole form
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+            //TODO: save to database
+            return RedirectToPage("/Index");
+        }
+    }
+}
